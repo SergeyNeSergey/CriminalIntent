1e29a3c4f1bcd57618fb5197aa9fc77943c76422
SergeyNeSergey <sergejnesergej15@gmail.com>
2020-06-20 13:29:16 +0400

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.criminalintent;
+
+
+
+import androidx.fragment.app.Fragment;
+
+
+public class CrimeActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
+
