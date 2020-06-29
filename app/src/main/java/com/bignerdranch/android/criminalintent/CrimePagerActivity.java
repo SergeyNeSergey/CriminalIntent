b6f051bed6112a8201e457422374b76a2b3e2480
SergeyNeSergey <sergejnesergej15@gmail.com>
2020-06-29 19:16:31 +0400

    added to first and to finish buttons

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -3,6 +3,8 @@ package com.bignerdranch.android.criminalintent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,8 @@ import java.util.UUID;
 public class CrimePagerActivity extends AppCompatActivity {
     private ViewPager2 mViewPager;
     private List<Crime> mCrimes;
+    private Button mToFirst;
+    private Button mToLast;
     private static final String EXTRA_CRIME_ID =
             "com.bignerdranch.android.criminalintent.crime_id";
     public static Intent newIntent(Context packageContext, UUID crimeId) {
@@ -32,6 +36,9 @@ public class CrimePagerActivity extends AppCompatActivity {
                 .getSerializableExtra(EXTRA_CRIME_ID);
         mViewPager = findViewById(R.id.crime_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
+        mToFirst=findViewById(R.id.button_first_crime);
+
+        mToLast=findViewById(R.id.button_last_crime);
         FragmentManager fragmentManager = getSupportFragmentManager();
         mViewPager.setAdapter(new FragmentStateAdapter(fragmentManager,getLifecycle()) {
 
@@ -47,12 +54,58 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return CrimeFragment.newInstance(crime.getId());
             }
         });
+
+
         for (int i = 0; i < mCrimes.size(); i++) {
             if (mCrimes.get(i).getId().equals(crimeId)) {
                 mViewPager.setCurrentItem(i);
                 break;
             }
+
+
         }
+
+
+
+
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                if (position == 0) {
+                    mToFirst.setEnabled(false);
+                } else {
+                    mToFirst.setEnabled(true);
+                }
+                if (position == mCrimes.size() - 1) {
+                    mToLast.setEnabled(false);
+                } else {
+                    mToLast.setEnabled(true);
+                }
+            }
+
+        });
+        mViewPager.refreshDrawableState();
+
+        mToFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+        mToLast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(mCrimes.size()-1);
+            }
+        });
+
+
+    }
+
 }
 
